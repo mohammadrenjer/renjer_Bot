a3240d03938d3ce953061a297ba1e98108b131d9
mohammadrenjer <mohammadjfr379@yahoo.com>
2016-04-21 12:22:37 +0430

    renjer

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {110453176,202623781},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
